fc42826bdc310f80b0b6302e5f0ff9aae5aad4f7
RajanNalawade <raj.nalawade2337@gmail.com>
2026-06-02 19:08:30 +0530

    Added Basics of Layout, Material Design 3

--- app/src/main/java/com/example/composetutorials/MainActivity.kt
+++ app/src/main/java/com/example/composetutorials/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.composetutorials
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorials.ui.theme.ComposeTutorialsTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,34 +32,62 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            /*ComposeTutorialsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            ComposeTutorialsTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message(author = "Android", body = "Jetpack Compose"))
                 }
-            }*/
-            Text(
-                text = "Hello Compose",
-                modifier = Modifier.fillMaxSize()
-            )
+            }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeTutorialsTheme {
-        Greeting("Android")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 40.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Adding images",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = "${msg.author}",
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.labelLarge
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = "${msg.body}",
+                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
     }
 }
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    ComposeTutorialsTheme {
+        Surface {
+            MessageCard(Message("Lexi", "Take a look at Jetpack Compose, it's great!"))
+        }
+    }
+}
+
+data class Message(
+    val author: String, val body: String
+)
